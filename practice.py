1f55fa42296e017624f1ac516901b1a01b914de9
Rushvez <ruslan.shvecow@yandex.ru>
2021-02-13 18:25:23 +0300

    practice

---
+++ practice.py
@@ -0,0 +1,16 @@
+money = input("Введите сумму депозита:")
+money = int(money)
+
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+deposit = []
+
+deposit.append(per_cent['ТКБ'] * money / 100)
+
+deposit.append(per_cent['СКБ'] * money / 100)
+
+deposit.append(per_cent['ВТБ'] * money / 100)
+
+deposit.append(per_cent['СБЕР'] * money / 100)
+
+print("Максимальная сумма, которую вы можете заработать —", int(max(deposit)), "руб.")
